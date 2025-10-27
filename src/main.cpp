cc19eb85feaa337469d69080f9e14d3a552494b6
Viktor Tsymbalyuk <Viktor.Tsymbalyuk@gmail.com>
2025-10-27 15:43:08 +0200

    test2

--- src/main.cpp
+++ src/main.cpp
@@ -116,7 +116,7 @@ void setup() {
   targetTempHA.setMin(15.0f);  // not variables
   targetTempHA.setMax(60.0f);
   targetTempHA.setMode(HANumber::ModeSlider);
-  // targetTempHA.setRetain(true);
+  targetTempHA.setRetain(true);
   targetTempHA.onCommand(onNumberCommand);
 
   ADCInput.setName("ADCInput");
